3b52f905abd9e83f0866e55047597daaca11b49d
Karoly <hornyal.mobile@gmail.com>
2022-08-28 16:00:51 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // https://www.weatherapi.com/
-// auto location detection
+// auto location detection?
 
 let baseUrl = "https://api.weatherapi.com/v1/";
 let apiMethod = "current.json";
@@ -9,7 +9,7 @@ let aqi = "no";
 console.log(baseUrl + apiMethod + "?key=" + key + "&q=" + myLocation + "&aqi=" + aqi);
 
 let url = () => {
-    let myNewLocation = document.querySelector(".new-location").value;
+    let myNewLocation = document.querySelector("#new-location-name").value;
     if (myNewLocation === "") {
         return (
             baseUrl + apiMethod + "?key=" + key + "&q=" + myLocation + "&aqi=" + aqi
@@ -21,110 +21,80 @@ let url = () => {
     }
 };
 
-function fetch_data() {
-    fetch(url())
-        .then(response => response.json())
-        .then(response => {
-            const lastUpdate = response.current.last_updated;
-            const updateTime = lastUpdate.slice(
-                lastUpdate.length - 5,
-                lastUpdate.length
-            );
+async function fetch_data() {
+    try {
+        await fetch(url())
+            .then(response => response.json())
+            .then(response => {
+                let lastUpdateTime = response.current.last_updated;
+                lastUpdateTime = lastUpdateTime.slice(
+                    lastUpdateTime.length - 5,
+                    lastUpdateTime.length
+                );
+                document.querySelector('#last-update-time').innerHTML = 'Last update: ' + lastUpdateTime;
 
-            const iconPath = "https:" + response.current.condition.icon;
-            document.querySelector('#favicon').setAttribute('href', iconPath);
+                const iconPath = "https:" + response.current.condition.icon;
+                document.querySelector('#favicon').setAttribute('href', iconPath);
 
-            const temperature = response.current.temp_c;
+                const locationName = response.location.name;
+                document.querySelector('title').innerHTML = locationName;
 
-            const windSpeed = response.current.wind_kph;
+                const temperature = response.current.temp_c;
+                document.querySelector('#temperature').innerHTML = temperature + '°c';
 
-            const condition = response.current.condition.text;
+                const windSpeed = response.current.wind_kph;
+                document.querySelector('#wind-speed').innerHTML = windSpeed + 'km/h';
 
-            const place = response.location.name;
-            document.querySelector('title').innerText = place;
+                document.querySelector('#weather-icon').setAttribute('src', iconPath);
 
-            const region = response.location.region;
+                const windDegree = response.current.wind_degree;
+                document.querySelector("#wind-degree").style.transform = `rotate(${windDegree}deg)`;
 
-            const feelsLike = response.current.feelslike_c;
+                const weatherCondition = response.current.condition.text;
+                document.querySelector('#weather-condition').innerHTML = weatherCondition + ',';
 
-            let country = response.location.country;
-            if (["Hongrie", "Ungarn", "Hungría"].includes(country)) {
-                country = "Hungary";
-            } else if (country === "UK") {
-                country = "United Kingdom";
-            }
+                document.querySelector("#location-name").innerHTML = locationName;
 
-            const windDegree = response.current.wind_degree;
+                const feelsLike = response.current.feelslike_c;
+                document.querySelector("#feels-like").innerHTML = 'feels: ' + feelsLike + '°c';
 
-            const weather = `
-    <div class="time-box">
-    <span class="update-time">Update: ${updateTime}</span>
-    </div>
-    <div class="temperature-box">
-    <img class="weather-icon" src="${iconPath}" alt="weather icon" />
-    <img class="thermometer" src="./img/thermometer.png" alt="thermometer icon" />
-    <span class="temperature">${temperature}°c</span>
-    </div>
-    
-    <div class="wind-box">
-    <img class="windsock" src="./img/windsock.png" alt="windsock"></img>
-    <span class="speed">${windSpeed} km/h</span>
-    <img class="arrow" src="./img/arrow.png" alt="arrow icon"></img>
-    </div>
-    
-    <div class="condition-box">
-    <span class="condition">${condition}</span>
-      </div>
-      
-      <div class="location-box">
-      <img class="location-icon" src="./img/location.png" alt="location icon" />
-      <span class="city">
-      ${place}
-      <span class="feels-like">Feels like ${feelsLike}°c</span>
-      </span>
-      </div>
-      
-      <div class="country-box">
-      <span class="region">${region} </span>
-      <span class="country">${country}</span>
-      </div>
-      `;
+                const regionName = response.location.region + ',';
+                document.querySelector("#region-name").innerHTML = regionName;
 
-            const container = document.querySelector(".container");
-            container.innerHTML = weather;
+                let countryName = response.location.country;
+                if (["Hongrie", "Ungarn", "Hungría"].includes(countryName)) {
+                    countryName = "Hungary";
+                } else if (countryName === "UK") {
+                    countryName = "United Kingdom";
+                }
+                document.querySelector("#country-name").innerHTML = countryName;
 
-            const windDirection = document.querySelector(".arrow");
-            windDirection.style.transform = `rotate(${windDegree}deg)`;
 
-            const city = document.querySelector(".city");
-            const myNewLocation = document.querySelector(".new-location");
-
-            city.addEventListener("click", () => {
-                myNewLocation.style.display = "block";
-                myNewLocation.focus();
+                document.querySelector("#location-name").addEventListener("click", () => {
+                    console.log('click');
+                    document.querySelector("#new-location-name").style.display = "block";
+                    document.querySelector("#new-location-name").focus();
+                });
             });
-        });
+    } catch (err) {
+        console.log(err);
+    } finally {
+        console.log('Fetch finished');
+    }
 }
 
 fetch_data();
 
-const myNewLocation = document.querySelector(".new-location");
-myNewLocation.addEventListener("keyup", (event) => {
-    if (myNewLocation !== "" && event.keyCode === 13) {
+const newLocationName = document.querySelector("#new-location-name");
+
+newLocationName.addEventListener("keyup", (event) => {
+    if (newLocationName !== "" && event.keyCode === 13) {
         fetch_data();
         event.target.style.display = "none";
     }
 });
 
-myNewLocation.addEventListener("focusout", (event) => {
+newLocationName.addEventListener("focusout", (event) => {
     event.target.style.display = "none";
 });
 
-/*
-const refresh_btn = document.querySelector(".refresh");
-refresh_btn.addEventListener("click", () => {
-    if (myNewLocation !== "") {
-        fetch_data();
-    }
-});
-*/
--- js/script.js
+++
@@ -1,7 +0,0 @@
-let baseUrl = "https://api.weatherapi.com/v1/";
-let apiMethod = "current.json";
-let key = "bfde22645c6c43d3a3e224519220804";
-let loc = "sw181eg";
-let aqi = "no";
-console.log(baseUrl + apiMethod + "?key=" + key + "&q=" + loc + "&aqi=" + aqi);
-
